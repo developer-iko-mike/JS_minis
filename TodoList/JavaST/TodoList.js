f73989c2b2e9d734bfba3d4f9e7bf1ebd4341d32
mohammad iko <developer.iko.mike@gmail.com>
2025-07-17 22:58:29 -0700

    responive all mini projects

--- TodoList/JavaST/TodoList.js
+++ TodoList/JavaST/TodoList.js
@@ -7,39 +7,49 @@ let emptyWrong = $.querySelector('.empty__wrong');
 let toCloseWrong = $.querySelector('.toClose__wrong');
 let addBox = $.querySelector('.hidden_box')
 
-input.addEventListener('keydown', function(event){
-if (event.code === "Enter" || event.code === 'NumpadEnter'){
-        if (!input.value.trim()){
-         emptyWrong.style.display = "block";
-         toCloseWrong.style.display = "none";
-        } else if (input.value.trim().length > 25) {
-          toCloseWrong.style.display = "block";         
-          emptyWrong.style.display = "none";         
-        } else {
-         let newDivTodo = $.createElement('div')
-         newDivTodo.classList.add('todo__items');
+input.addEventListener('keydown', function (event) {
+  if (event.code === "Enter" || event.code === 'NumpadEnter') {
+    if (!input.value.trim()) {
+      emptyWrong.style.display = "block";
+      toCloseWrong.style.display = "none";
+    } else if (input.value.trim().length > 25) {
+      toCloseWrong.style.display = "block";
+      emptyWrong.style.display = "none";
+    } else {
+      let newDivTodo = $.createElement('div')
+      newDivTodo.classList.add('todo__items');
 
-         let newtitleTodo = $.createElement('p')
-         newtitleTodo.classList.add('todo__item');
-         newtitleTodo.innerHTML = input.value.trim()
+      let newtitleTodo = $.createElement('p')
+      newtitleTodo.classList.add('todo__item');
+      newtitleTodo.innerHTML = input.value.trim()
 
-         let removeImgNewTodo = $.createElement('img')
-         removeImgNewTodo.classList.add('todo__image');
-         removeImgNewTodo.src = './Assets/recycleBin.png'
-         removeImgNewTodo.addEventListener('click', function(event){
-          event.target.parentElement.remove()
-         })
+      let removeImgNewTodo = $.createElement('img')
+      removeImgNewTodo.classList.add('todo__image');
+      removeImgNewTodo.src = './Assets/recycleBin.png'
+      removeImgNewTodo.addEventListener('click', function (event) {
+        event.target.parentElement.remove()
+      })
 
-         mainTodo.append(newDivTodo)
-         newDivTodo.append(newtitleTodo , removeImgNewTodo)
-         addBox.style.right = '1rem'
-         
-         input.value = ''
-         emptyWrong.style.display = "none";
-         toCloseWrong.style.display = "none"; 
-         setTimeout(function(){
-          if (addBox.style.right = '1rem'){
-           addBox.style.right = '-30rem'
+      mainTodo.append(newDivTodo)
+      newDivTodo.append(newtitleTodo, removeImgNewTodo)
+      if (window.innerWidth < 768) {
+        addBox.style.bottom = '1rem'
+      } else {
+        addBox.style.right = '1rem'
+      }
+
+      input.value = ''
+      emptyWrong.style.display = "none";
+      toCloseWrong.style.display = "none";
+      setTimeout(function () {
+        if (addBox.style.right = '1rem') {
+          if (window.innerWidth < 768) {
+            addBox.style.bottom = '-30rem'
+          } else {
+            addBox.style.right = '-30rem'
           }
-         },3000)
-        }}})
+        }
+      }, 3000)
+    }
+  }
+})
